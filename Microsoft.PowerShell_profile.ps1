538a6b1c74e275d9ef945fe4349cc174bc4d8ec4
Quentin Auster <qauster1@gmail.com>
2022-06-05 19:31:07 -0400

    Powershell profile and ohmyposh config files

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,6 @@
+# oh-my-posh init pwsh --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/jandedobbeleer.omp.json' | Invoke-Expression
+
+oh-my-posh init pwsh --config 'C:\Users\qaust\OneDrive\Documents\PowerShell\qauster.omp.json' | Invoke-Expression
+
+Import-Module -Name Terminal-Icons
+
